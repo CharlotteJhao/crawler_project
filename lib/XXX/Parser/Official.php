c784a6d2c74141064829b87eb30e6617816e47a8
Charlotte <crazycat2442@gmail.com>
2016-11-02 02:51:13 +0800

    Add Billboard & Official parser implement IParser

---
+++ lib/XXX/Parser/Official.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace XXX\Parser;
+
+class Official implements IParser
+{
+    public function getHtmlObject($htmlObj)
+    {
+    }
+
+    public function getChartDate($htmlObj)
+    {
+    }
+
+    public function getPosition($pq)
+    {
+    }
+
+    public function getArtist($pq)
+    {
+    }
+
+    public function getTitle($pq)
+    {
+    }
+
+    public function getSpotifyId($pq)
+    {
+    }
+
+    public function getRowData($pq, $meta_type)
+    {
+    }
+}
