c5ca8753e0d7f3f197326e1bc21ab5160457a989
Charlotte <crazycat2442@gmail.com>
2016-11-02 08:35:10 +0800

    Get chart list by a new way

--- utility/crawler_record_charts.php
+++ utility/crawler_record_charts.php
@@ -1,12 +1,25 @@
 <?php
 
-use XXX\Crawler\Billboard;
-use XXX\Crawler\Official;
+use XXX\Crawler;
+use XXX\Parser\Billboard;
+use XXX\Parser\Official;
 
+date_default_timezone_set('Asia/Taipei');
 require __DIR__ . '/../vendor/autoload.php';
 
-$o = new Official();
-$o_list = $o->getChartList('track');
+$c = new Crawler();
+$c->setDomain('http://www.billboard.com/charts/hot-100');
+$c->setChartType('track');
+$c->setParser(new Billboard());
+$date = $c->getChartDate();
+$list = $c->getChartList();
+var_dump($date);
+var_dump($list);
 
-$b = new Billboard();
-$b_list = $b->getChartList('track');
+$c->setParser(new Official());
+$c->setDomain('http://www.officialcharts.com/charts/singles-chart/');
+$c->setChartType('track');
+$date = $c->getChartDate();
+$list = $c->getChartList();
+var_dump($date);
+var_dump($list);
